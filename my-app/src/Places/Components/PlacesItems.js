ca619acd6108123972b2ecb9d10d8107ca31d832
Nabil1907 <snabil084@gmail.com>
2020-12-16 01:36:48 +0200

    Adding Delete Button

--- my-app/src/Places/Components/PlacesItems.js
+++ my-app/src/Places/Components/PlacesItems.js
@@ -7,8 +7,19 @@ import Modal from '../../Shared/Components/UIElements/modal';
 import Map from '../../Shared/Components/UIElements/Map';
 const PlacesItem = props => {
     const [showMap, setShowMap] = useState(false);
+    const [showConfirmModel,setShowConfirmModel] = useState(false);
     const openMapHandler = () => setShowMap(true);
     const closeMapHandler = () => setShowMap(false);
+    const showDeleteWarning =() =>{
+      setShowConfirmModel(true) ;
+    }
+    const cacelDelete = () =>{
+      setShowConfirmModel(false);
+    }
+    const confirmDelete = () =>{
+      setShowConfirmModel(false);
+      console.log('Deleting');
+    }
         return (
             <React.Fragment>
             <Modal
@@ -25,6 +36,23 @@ const PlacesItem = props => {
                </Map>
               </div>
             </Modal>
+            <Modal
+            show={showConfirmModel}
+            onCancel={cacelDelete}
+             header="Are You Sure ?"
+             footerClass="place-item__modal-actions"
+             footer={
+               <React.Fragment>
+                 <ButtonItems inverse onClick={cacelDelete}>CLOSE</ButtonItems>
+                 <ButtonItems danger onClick={confirmDelete}>DELETE</ButtonItems> 
+               </React.Fragment>
+             }
+
+            >
+              <p>
+                Do you Sure delete this place ? please note this can't be undone there after . 
+              </p>
+            </Modal>
         <div class="card">
         <img src={props.image} alt={props.title} />
               <div class="container">
@@ -35,7 +63,7 @@ const PlacesItem = props => {
                <div className="place-buttons">
                     <ButtonItems inverse onClick={openMapHandler} >View On Map</ButtonItems>
                     <ButtonItems to={`/places/${props.id}`}>Edit</ButtonItems>
-                    <ButtonItems danger>Delete</ButtonItems>
+                    <ButtonItems danger onClick={showDeleteWarning} >Delete</ButtonItems>
                 </div>
         </div>
         </React.Fragment>
